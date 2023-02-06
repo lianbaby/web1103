0d17760ead8949d5703cbbdbed56f2061ef3cb11
lian <elisaxha@gmail.com>
2023-02-06 11:17:28 +0800

    完成簡易首頁排版

--- front/main.php
+++ front/main.php
@@ -1,24 +1,148 @@
+<style>
+#poster{
+  width: 420px;
+  height: 400px;
+  position: relative;
+}
+
+.pos{
+ width: 210px; 
+ height: 280px;
+ /* background-color: white; */
+ margin-left: 105px;
+ position: absolute;
+ text-align: center;
+ display: none;
+}
+
+.pos img{
+  width: 100%;
+  height: 260px;
+}
+
+.controls{
+  width: 420px;
+  height: 110px;
+  /* background-color: lightyellow; */
+  margin: 10px auto 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  position: absolute;
+  bottom: 0;
+}
+
+/* .left,.right{
+  width: 40px;
+  height: 40px;
+  background-color: red;
+} */
+
+.left,.right{
+  /* transparent透明色 */
+  border-top:20px solid transparent ;
+  border-bottom:20px solid transparent ;
+}
+
+.left{
+  border-right:20px solid black ;
+}
+
+.right{
+  border-left:20px solid black;
+}
+.btns{
+  width: 320px;
+  height: 100px;
+  /* background-color: green; */
+  display: flex;
+  overflow: hidden;/*hidden超出範圍的部份*/
+}
+
+.btn{
+  width: 80px;
+  font-size: 12px;
+  text-align:center;
+  flex-shrink: 0;/*保持設定的寬度，不要跑掉 */
+  box-sizing: border-box;
+}
+.btn img{
+  width: 100%;
+  height: 80px;
+}
+
+
+</style>
 <div class="half" style="vertical-align:top;">
       <h1>預告片介紹</h1>
       <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-          <ul class="lists">
-          </ul>
-          <ul class="controls">
-          </ul>
+        <div id="poster">
+          <div class="lists">
+            <?php
+              $posters=$Trailer->all(['sh'=>1]);
+              foreach($posters as $poster){
+
+                ?>
+            <div class="pos">
+              <img src="./upload/<?=$poster['img'];?>" alt="">
+            </div>
+            <?php
+            }
+            ?>
+          </div>
+          <div class="controls">
+            <div class="left" onclick="pp(1)"> </div>
+            <div class="btns">
+            <?php
+              $posters=$Trailer->all(['sh'=>1]);
+              foreach($posters as $key => $poster){
+
+                ?>
+              <div class="btn" id="pos<?=$key;?>">
+                <img src="./upload/<?=$poster['img'];?>" alt="">
+                <div><?=$poster['name'];?></div>
+              </div>
+              <?php
+              }
+              ?>
+            </div>
+            <div class="right" onclick="pp(2)"></div>
+          </div>
         </div>
       </div>
     </div>
 
+    <script> 
+      $(".pos").eq(0).show(); //只秀出第一張海報
+
+      var nowpage=0,num=<?=count($posters);?>; //第1題的第二個版。copy過來用(程式要有小修改)
+							function pp(x)
+							{
+								var s,t;
+								if(x==1&&nowpage-1>=0)
+								{nowpage--;}
+								if(x==2 && (nowpage+1)<=(num*1-4)) 
+								{nowpage++;}
+								$(".btn").hide()
+								for(s=0;s<=3;s++)
+								{
+									t=s*1+nowpage*1;
+									$("#pos"+t).show()
+								}
+							}
+							pp(1)
+
+    </script>
 
     <div class="half">
       <h1>院線片清單</h1>
       <div class="rb tab" style="width:95%;">
-        <div>
+      <!-- display:flex橫排，wrap自動換行 -->
+        <div style="display:flex;flex-wrap:wrap"> 
           <?php
             $today=date("Y-m-d");
             $ondate=date("Y-m-d",strtotime(("-2 days")));
-            $all=q("select count(*) as 'total' from `movie` where `sh`=1 && `ondate` between 'ondate' AND '$today'")[0]['total'];
+            $all=$Movie->count(" where `sh`=1 && `ondate` between '$ondate' AND '$today'");
             $div=4;
             $pages=ceil($all/$div);
             $now=$_GET['p']??1;
